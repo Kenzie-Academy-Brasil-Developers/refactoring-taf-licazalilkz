1c2c491fb5bbfb3e2bd85fbf234b6a4cc78f8917
licazalilkz <licazalilkz>
2023-03-02 17:51:43 -0300

    feito

--- script.js
+++ script.js
@@ -1,11 +1,88 @@
 // Implemente aqui as funções
+function validGender(genero){
+    if(genero === "male"){
+       return true;
+    }
+    else if(genero === "female"){
+       return true;
+    }
+    return false
+}
 
+function validHeight(genero, altura){
+    if(genero === 'male'){
+        if(altura > 1.69){
+            return true;
+        }
+    }
+    if(genero === 'female'){
+        if(altura > 1.59){
+            return true;
+        }
+    }
+    return false;
+}
 
+function validAbs(abs, genero){
+    if(genero === 'male' && abs > 40){
+        return true;
+    }
+    else if(genero === 'female' && abs > 40){
+        return true;
+    }
+   return false;
+}
 
+function validSwin(metrosNado, tempoNado, secMergulho, genero){
+    if(genero === 'male' && metrosNado > 99 && tempoNado < 61 || secMergulho>29){
+        return true;
+    }
+    else if(genero === 'female' && metrosNado > 99 && tempoNado < 61 || secMergulho > 29){
+        return true;
+    }
+    return false
+}
 
+function validBarTest(barras, secBarra, genero){
+    if(genero === 'male'){
+        if(barras > 5 || secBarra < 16){
+            return true;
+        }
+    }
+    else if(genero === 'female'){
+        if(barras > 4 || secBarra < 13){
+            return true;
+        }
+    }
+    return false;
+}
 
-function areCandidateResultsValid() {
+function validRun(corrida, tempoCorrida, genero){
+    if(genero === 'male'){
+        if(corrida > 2999 && tempoCorrida < 721 || corrida > 4999 && tempoCorrida < 1201 ){
+            return true;
+        }
+    }
+    else if(genero === 'female'){
+        if(corrida > 3999 && tempoCorrida < 901 || corrida > 5999 && tempoCorrida < 1321 ){
+            return true;
+        }
+    }
+    return false;
+}
 
+function areCandidateResultsValid(genero, altura, barras, secBarra, abs, corrida, tempoCorrida, metrosNado, tempoNado, secMergulho){
+    if(        
+        validGender(genero) && 
+        validHeight(genero, altura) && 
+        validAbs(abs, genero) &&
+        validSwin(metrosNado, tempoNado,secMergulho,genero) &&
+        validBarTest(barras,secBarra,genero) &&
+        validRun(corrida,tempoCorrida,genero)
+    ){
+        return true;
+    }
+    return false;
 }
 
 // Chamando a função com os parametros recebidos via script de teste
@@ -24,3 +101,8 @@ const areCandidateValid = areCandidateResultsValid(
 );
 
 // Chame aqui a função que mostra o resultado no console
+function showMessage(message){
+    console.log(message.toString().toUpperCase());
+}
+
+showMessage(areCandidateValid);
